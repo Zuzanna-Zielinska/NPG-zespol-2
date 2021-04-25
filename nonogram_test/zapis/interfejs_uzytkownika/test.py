341a6064ce103d19befa1228b0212cfbff63c2c2
kk2115 <80417519+kk2115@users.noreply.github.com>
2021-04-25 22:24:36 +0200

    Update test.py

--- nonogram_test/zapis/interfejs_uzytkownika/test.py
+++ nonogram_test/zapis/interfejs_uzytkownika/test.py
@@ -1,6 +1,14 @@
 import nonogram_test.zapis.interfejs_uzytkownika.gui_zapis as n
 import numpy as np
 
+
+# tutaj mozna zobaczyc jak dziala program
+# odpala sie gui_zapis, zaznaczamy nasz wzor
+# a po wcisnieciu przycisku "END" zamyka sie okno, a my
+# mozemy wykorzystac otrzymana macierz.
+
+
+# ponizej jest program wypisujacy otrzymana macierz
 list_as_array = np.array(n.answer_nonogram)
 
 print(list_as_array)
